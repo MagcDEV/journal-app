4227b7649b9fba75672665df919113d1c9a2e91a
magcdev <manuel117g@gmail.com>
2023-03-09 13:30:02 -0400

    working in auth

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,11 +1,13 @@
 import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { ImageGallery } from "../components/ImageGallery";
 
 export const NoteView = () => {
   return (
     <Grid
       container
       direction="row"
+      alignItems="center"
       justifyContent="space-between"
       sx={{ mb: 1 }}
     >
@@ -15,12 +17,33 @@ export const NoteView = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Button>
-            <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
-            Guardar
+        <Button sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Guardar
         </Button>
-
       </Grid>
+      <Grid container>
+        <TextField
+          variant="filled"
+          type="text"
+          fullWidth
+          placeholder="Ingrese un Titulo"
+          label="Titulo"
+          sx={{ border: "none", mb: 1 }}
+        />
+
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="Que sucedio el dia de hoy?"
+          minRows={5}
+        />
+      </Grid>
+
+      <ImageGallery />
+      {/* Image Gallery */}
     </Grid>
   );
 };
